3ac11bed55f5a16bc51a4a0a79f2c805aa28265e
forthtemple <glinde@forthtemple.com>
2017-03-27 16:59:39 +1300

    Forth temple models

---
+++ data/scripts/fetch_forthtemple_models.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+cd $DIR
+
+FILE=forthtemple_faster_rcnnmasgv_iter_20000.caffemodel
+URL=https://www.dropbox.com/s/4k7ppr720pdao3j/$FILE
+
+
+if [ -f $FILE ]; then
+  echo "File already exists. "
+ 
+fi
+
+echo "Downloading pretrained Forthtemple model..."
+
+wget $URL -O $FILE
+
+echo "Done."
